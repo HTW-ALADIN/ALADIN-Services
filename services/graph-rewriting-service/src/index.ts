e7d733741cf5b8bec2d984d851d0dfb6415de1e1
Paul Christ <paul.l.christ@web.de>
2026-02-18 20:58:37 +0100

    feat: add Dockerfile for shipping

--- services/graph-rewriting-service/src/index.ts
+++ services/graph-rewriting-service/src/index.ts
@@ -1,7 +1,7 @@
 import { buildServer } from './app';
 const server = buildServer();
 
-server.listen({ port: 8080 }, (err, address) => {
+server.listen({ port: 8080, host: '0.0.0.0' }, (err, address) => {
 	if (err) {
 		console.error(err);
 		process.exit(1);
